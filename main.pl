2f4c6efbf530fb731b95cb2658c75e013df4a0e0
Amr Alaa <n1amr1@gmail.com>
2017-06-10 22:59:42 +0200

    implement board representation

--- main.pl
+++ main.pl
@@ -1,4 +1,24 @@
+initial_state([[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]).
+
+print_board([L1, L2, L3]) :-
+    print_line_separator, print_board_line(L1),
+    print_line_separator, print_board_line(L2),
+    print_line_separator, print_board_line(L3),
+    print_line_separator.
+
+print_line_separator :- write('-------------'), nl.
+
+print_board_line(Cells) :- writef('| %w | %w | %w |\n', Cells).
+
+set_cell(S, NS, [X, Y], Symbol) :-
+    append(AL, [Line|BL], S), length(AL, X), % get Xth line
+    append(AC, [_|BC], Line), length(AC, Y), % get Yth cell
+    append(AC, [Symbol|BC], NLine),
+    append(AL, [NLine|BL], NS).
 
 main :-
-
+    initial_state(S),
+    print_board(S),
+    set_cell(S, NS, [2, 2], 'x'),
+    print_board(NS),
     !.
